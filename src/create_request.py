1df0f6430a0868371bd5903c0dabac6086877a63
19521242bao <19521242@gm.uit.edu.vn>
2023-09-01 23:05:46 +0700

    update code for create test request

---
+++ src/create_request.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import json 
+
+
+
+df=pd.read_csv("data/val.csv")
+
+n=len(df)
+columns=df.columns.tolist()
+#create 100 requests
+request_list=[]
+for i in range(100):
+    id=np.random.randint(1000,100000)
+    num_data=np.random.randint(1,n//2)
+    data=df.sample(num_data)
+    #convert to list
+    data=data.values.tolist()
+    request={
+        "id":id,
+        "data":data,
+        "columns":columns
+    }
+    print(request)
+    save_path=f"requests/request_{id}.json"
+    with open(save_path,"w") as f:
+        json.dump(request,f,indent=4)
