74b315dca537fe7a7f8e69673ccec62f7fbe0ce6
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2023-09-26 13:41:52 +0700

    update_test

---
+++ tests/test_model_correctness.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np
+import yaml
+import logging
+import time
+import joblib
+
+MODEL_DIR = "models"
+def test_model_correctness():
+    scaler=joblib.load(f"{MODEL_DIR}/scaler.pkl")
+    clf = joblib.load(f"{MODEL_DIR}/diabetes_model.pkl")
+    data = [-1.15332192, -0.05564105,  0.12035144, -1.25882277, -1.08285125,
+        -0.28446352, -0.49468374, -0.52559768]
+    x= np.array(data).reshape(-1, 8)
+    x=scaler.transform(x)
+    pred=clf.predict(x)[0]
+    assert pred == 0
